e253ccb50c803e037becaf1dc4bbcdf8e766d3b5
Lizzie Anthony <lizziewrab@me.com>
2021-10-30 16:31:45 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+//const main = document.getElementById('main')
+const main = document.querySelector('#main')
+//console.log(main); // to find
+main.remove()
+//document.body.removeChild(main)
+
+const newHeader = document.createElement('h1')
+ newHeader.id = 'victory'
+ //newHeader.setAttribute('id", 'victory") meh 
+
+newHeader.innerHTML = 'Liz is the champion'
